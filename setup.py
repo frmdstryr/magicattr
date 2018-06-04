8d6b9e398cf92a840db6dc8f004fac0717c2cced
frmdstryr <frmdstryr@protonmail.com>
2018-06-04 17:29:55 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='magicattr',
-    version='0.1.0',
+    version='0.1.1',
     author='frmdstryr',
     author_email='frmdstryr@gmail.com',
     url='https://github.com/frmdstryr/magicattr',
